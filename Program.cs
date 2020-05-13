b7a650f85cd221448d775245cd233397c8dd452d
DestBreak <rey.hadiansyah7th@gmail.com>
2020-05-13 23:08:29 +0700

    Add - Program.cs

--- Program.cs
+++ Program.cs
@@ -6,5 +6,32 @@ using System.Threading.Tasks;
 
 namespace PolymorphismInterface
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
 
+            IPrinterWindows printer;
+            Console.WriteLine("Pilih Printer : ");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet");
+            Console.WriteLine("Nomor Printer [1..3] : ");
+            int printerNumber = Convert.ToInt32(Console.ReadLine());
+
+            if(printerNumber == 1) {
+                printer = new Epson();
+            }else if(printerNumber == 2) {
+                printer = new Canon();
+            }else {
+                printer = new LaserJet();
+            }
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+        }
+    }
 }
